4e743813184a920f68ac030e0fa6c7bb7711a375
Kevin Lin <kevin034@gmail.com>
2017-01-30 09:55:46 -0800

    Add files via upload

---
+++ GettbackBackIntoIt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayApplication
+{
+   class MyArray
+   {
+      static void Main(string[] args)
+      {
+         int []  n = new int[10]; /* n is an array of 10 integers */
+         int i;
+         
+         n[0]=0;
+         Console.WriteLine(n[0]);
+         n[1]=1;
+         Console.WriteLine(n[1]);
+
+         /* initialize elements of array n */
+         for ( i = 2; i < 10; i++ )
+         {
+            n[ i ] = n[ i-1 ] + n[ i-2 ];
+            Console.WriteLine(n[i]);
+         }
+
+//         Console.ReadKey();
+      }
+   }
+}
